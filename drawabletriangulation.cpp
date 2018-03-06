38aa664f03d2fff9aaab8c6ac6d3eb034d7d9b3e
lilcaesar <lilcaesar4@gmail.com>
2018-03-06 15:42:23 +0100

    Creation DrawableTriangulation class

---
+++ drawabletriangulation.cpp
@@ -0,0 +1,6 @@
+#include "drawabletriangulation.h"
+
+DrawableTriangulation::DrawableTriangulation()
+{
+
+}
---
+++ drawabletriangulation.h
@@ -0,0 +1,11 @@
+#ifndef DRAWABLETRIANGULATION_H
+#define DRAWABLETRIANGULATION_H
+
+
+class DrawableTriangulation
+{
+public:
+    DrawableTriangulation();
+};
+
+#endif // DRAWABLETRIANGULATION_H
